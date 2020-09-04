870a4fe9b8b28d826b66a2cea9c55b36e9a731cf
lanshenzz <495102181@qq.com>
2020-09-04 09:06:04 +0800

    执行时间修改

--- src/main/java/com/teacher/scholat/util/ScheduledTask.java
+++ src/main/java/com/teacher/scholat/util/ScheduledTask.java
@@ -11,12 +11,12 @@ public class ScheduledTask {
     @Autowired
     private StatisticController statisticController;
 
-    @Scheduled(cron = "0 0 0 ? * *")
+    @Scheduled(cron = "0 0 0 * * ?")
     public void task1() {
         statisticController.total();
     }
 
-    @Scheduled(cron = "0 0 1 ? * *")
+    @Scheduled(cron = "0 0 1 * * ?")
     public void taskGetAllPaper() {
         statisticController.getAllPaper();
     }
